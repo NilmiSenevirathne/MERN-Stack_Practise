fa561d7315b3b7251fc16dfc8caed64930beac31
Nilmi Ama Senevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2025-03-02 13:29:49 +0530

    Add files via upload

---
+++ src/Users.js
@@ -0,0 +1,34 @@
+import {Box} from "@mui/material";
+import UserForm from "./UserForm";
+import UsersTable from "./UsersTable";
+
+
+const users =[
+    {
+        id :1,
+        name: 'Prasad'
+    },
+    {
+        id: 2,
+        name: 'Ama',
+    }
+];
+
+const Users = () =>{
+
+    return(
+       <Box 
+          sx={{
+            width:'calc(100% - 100px)',
+            margin: 'auto',
+            marginTop: '100px'
+          }}
+       >
+         <UserForm/>
+         <UsersTable rows={users}/>
+       </Box>
+        
+    );
+
+}
+export default Users;
